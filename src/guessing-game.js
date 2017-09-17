865ac8fcbff24b23cbbc9d742554fe1bfcdb25c6
Olga <31817567+V-O-G@users.noreply.github.com>
2017-09-18 02:51:36 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        var x = Math.ceil((this.min + this.max)/2);
+        return parseInt(x)
     }
 
     lower() {
-
+        var x = Math.ceil((this.min + this.max)/2);
+        this.max = parseInt(x);
     }
 
     greater() {
-
+        var x = Math.ceil((this.min + this.max)/2);
+        this.min = parseInt(x);
     }
 }
 
